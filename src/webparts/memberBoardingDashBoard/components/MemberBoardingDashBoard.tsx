9a15b05531cf2099a104036c3436ee009dada642
NaveenReddyYapala <Yapalanaveenreddy7@gmail.com>
2026-05-22 09:38:54 +0530

    Updated commit with WebPart and sppkg file

--- src/webparts/memberBoardingDashBoard/components/MemberBoardingDashBoard.tsx
+++ src/webparts/memberBoardingDashBoard/components/MemberBoardingDashBoard.tsx
@@ -48,9 +48,11 @@ export default class MemberBoardingDashBoard extends React.Component<IMemberBoar
   }
   private onEditClick(item: IBoardingItem): void {
     if (item.Status === "Draft" || item.Status === "Assigned to Maker") {
-      window.location.href = `/sites/NaveenReddy/SitePages/MemberOnBoardingNewForm.aspx?ItemId=${item.Id}`;
+    //  window.location.href = `/sites/NaveenReddy/SitePages/MemberOnBoardingNewForm.aspx?ItemId=${item.Id}`;
+    window.location.href = `/sites/apps/MembershipBoarding/SitePages/MemberOnBoardingNewForm.aspx?ItemId=${item.Id}`;
     } else {
-      window.location.href = `/sites/NaveenReddy/SitePages/MemberBoardingForm.aspx?ItemId=${item.Id}`;
+    //  window.location.href = `/sites/NaveenReddy/SitePages/MemberBoardingForm.aspx?ItemId=${item.Id}`;
+    window.location.href = `/sites/apps/MembershipBoarding/SitePages/MemberBoardingForm.aspx?ItemId=${item.Id}`;
     }
   }
 
